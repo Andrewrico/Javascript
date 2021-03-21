489edf279486c414c434bf47fa01ef3f2a2e3569
andrewrico <andrewric86@gmail.com>
2021-03-21 08:59:55 -0400

    initial

--- Cookies.js
+++ Cookies.js
@@ -1,15 +1,19 @@
 window.addEventListener('load', (e) => {
 
-    let COOKIE_TRIGGER = '/pages/xplus.html' // target 
-    let COOKIE_KEY = "cookiefound" // key 
+    // set data
+    let COOKIE_TRIGGER = '/path/page.html' // target 
+    let COOKIE_KEY = "cookieKey" // key 
     let COOKIE_VALUE = false // value 
     let COOKIE_EXPIRES = 1000 * 36000 + 100000// expires
 
 
+    // only save cookie in this location
     if (window.location.pathname !== undefined && window.location.pathname === COOKIE_TRIGGER) {
 
+        // if location set value to true
         COOKIE_VALUE = true
 
+        // set Cookie
         const setCookie = function (COOKIE_VALUE) {
             if (COOKIE_VALUE = true) {
                 return document.cookie.indexOf(COOKIE_KEY + '=' + COOKIE_VALUE) !== -1;
@@ -17,6 +21,7 @@ window.addEventListener('load', (e) => {
             return false;
         };
 
+        // create cookie
         const createCookie = function (COOKIE_VALUE) {
             if (COOKIE_VALUE = true) {
                 let COOKIE_DATE = new Date();
@@ -27,13 +32,16 @@ window.addEventListener('load', (e) => {
             };
         };
 
-        let match = window.location.pathname.match(COOKIE_TRIGGER)
-        COOKIE_TRIGGER = match ? match[1] : undefined;
+        // match trigger and window location
+        let matchLocationTrigger = window.location.pathname.match(COOKIE_TRIGGER)
+        COOKIE_TRIGGER = matchLocationTrigger ? matchLocationTrigger[1] : undefined;
 
+        // save cookie to browser
         if (!createCookie(COOKIE_TRIGGER)) {
             setCookie(COOKIE_TRIGGER);
         };
 
+        // get cookie array
         let cookieArray = document.cookie
             .split(';')
             .map(cookie => cookie.split('='))
@@ -44,11 +52,15 @@ window.addEventListener('load', (e) => {
             {
         });
 
+        // log success
         console.log(cookieArray)
 
     } else {
 
+        // delete cookie if go to other location
         document.cookie = `${COOKIE_KEY}=${!COOKIE_VALUE}` +  "; expires=Thu, 12 Dec 2012 00:00:01 GMT";
+
+        //log
         console.log(`Not found!`);
     }
 
