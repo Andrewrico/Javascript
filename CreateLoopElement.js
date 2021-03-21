5e17fd6b55c35955105bb761584a1468ae74a0aa
andrewrico <andrewric86@gmail.com>
2021-03-21 13:40:28 -0400

    create element

--- CreateLoopElement.js
+++ CreateLoopElement.js
@@ -1,60 +1,81 @@
 window.addEventListener('load', (e) => {
 
-    const canvasNewElements = document.querySelector("#GenerateElements");
-    let elementName = 'luci';
-    let ElementTagOne = 'div'
-    let elementIdOne = 'carollina';
-    let elementCountOne = Number('10');
-
+    // set data
+    const domFactory = document.querySelector("#element-factory");
+    let elementName = 'newElement';
+    let elementTagOne = 'div'
+    let elementIdOne = 'first-group';
+    let elementCountOne = Number('100');
     let elementTagTwo = 'span'
-    let elementIdTwo = 'sofia';
-    let elementCountTwo = Number('10');
+    let elementIdTwo = 'second-group';
+    let elementCountTwo = Number('100');
 
+    // function create new elements
     const domNewElements = () => {
 
-        let newElement = this;
+        // get fragment
         let newFragment = new DocumentFragment();
 
-        for (let i = 1; i <= elementCountOne; i++) {
-            newElement = document.createElement(`${ElementTagOne}`);
-            newElement.setAttribute('id', `${elementName}-${elementIdOne}-${[i]}`);
-            newElement.setAttribute('class', `${elementName}`);
-            newFragment.append(newElement);
-        };
+        // if fragment is defined
+        if (newFragment !== undefined) {
 
-        for (let i = 1; i <= elementCountTwo; i++) {
-            newElement = document.createElement(`${elementTagTwo}`)
-            newElement.setAttribute('id', `${elementName}-${elementIdTwo}-${[i]}`);
-            newElement.setAttribute('class', `${elementName}`);
-            newFragment.append(newElement);
-        };
+            // create group one
+            for (let i = 1; i <= elementCountOne; i++) {
+                newElement = document.createElement(`${elementTagOne}`);
+                newElement.setAttribute('id', `${elementName}-${elementIdOne}-${[i]}`);
+                newElement.setAttribute('class', `${elementName}`);
+                newFragment.append(newElement);
+            };
 
-        return newFragment;
-    };
+            // create group two
+            for (let i = 1; i <= elementCountTwo; i++) {
+                newElement = document.createElement(`${elementTagTwo}`)
+                newElement.setAttribute('id', `${elementName}-${elementIdTwo}-${[i]}`);
+                newElement.setAttribute('class', `${elementName}`);
+                newFragment.append(newElement);
+            };
 
-    canvasNewElements.append(domNewElements());
+            // return fragment with groups
+            return newFragment;
+        }
+    }
 
+    // append created elements to the dom
+    domFactory.append(domNewElements());
+
+    // manipulate elements
     const domGetElements = () => {
 
-        let a = document.querySelectorAll(`[id*="${elementName}-${elementIdOne}-"][class*="${elementName}"]`);
-        let b = document.querySelectorAll(`[id*="${elementName}-${elementIdTwo}-"][class*="${elementName}"]`);
+        // groups
+        let groupOne = document.querySelectorAll(`[id*="${elementName}-${elementIdOne}-"][class*="${elementName}"]`);
+        let groupTwo = document.querySelectorAll(`[id*="${elementName}-${elementIdTwo}-"][class*="${elementName}"]`);
 
-        if (generateNewElements !== undefined) {
+        // if function create new elements is define
+        if (domNewElements !== undefined) {
 
-            for (let i = a.length - 1; i >= 0; i--) {
-                a[i].style.left = '30%'
+            // manipulate group one
+            for (let i = groupOne.length - 1; i >= 0; i--) {
+                groupOne[i].style.left = '30%'
             };
         
-            for (let i = b.length - 1; i >= 0; i--) {
-                b[i].style.right = '30%'
+            // manipulate group two
+            for (let i = groupTwo.length - 1; i >= 0; i--) {
+                groupTwo[i].style.right = '30%'
             };
+
+            // log
+            console.log(document.querySelectorAll(`[class*="${elementName}"]`))
 
         } else {
 
+            // unless criterial is fullfill return false
             return false;
         }
     }
 
-    domGetElements();
-  
+    // if domNewElements if defined return domGetElements
+    if (domNewElements == undefined || domGetElements !== null) {
+        domGetElements()
+    };
+
 });
