31ab58d8d4ccb00b98c7b20c7d438f73a52884f4
andrewrico <andrewric86@gmail.com>
2021-03-23 00:23:11 -0400

    factory fix

--- CreateNewHTMLFactory.js
+++ CreateNewHTMLFactory.js
@@ -3,8 +3,6 @@ window.addEventListener('load', () => {
   // data
   let factoryHTMLTag = 'div'
   let factoryId = 'factory'
-  let elements1;
-  let elements2;
 
   // parent div
   const createNewElement = () => {
@@ -47,19 +45,22 @@ window.addEventListener('load', () => {
     }
 
     // New HTML
-    elements1 = new newProtoObj(1, 'div', 'group1', 'group1 dynamicElement').createNewHTML();
-    elements2 = new newProtoObj(1, 'div', 'group2', 'group2 dynamicElement').createNewHTML();
+    elements1 = new newProtoObj(
+    count = 50,
+    tag = 'div',
+    id = 'ele1',
+    className = 'elementClass').createNewHTML();
 
-      // return fragment with groups
+    elements2 = new newProtoObj(
+    count = 50,
+    tag = 'div',
+    id = 'ele2',
+    className = 'elementClass').createNewHTML();
+
+    // return fragment with groups
     return newFragment;
 
-    } else {
-
-      // return false and log
-      console.log('Something went wrong!');
-      return false;
-
-    }
+    } 
 
   }
 
@@ -73,51 +74,44 @@ window.addEventListener('load', () => {
   const domGetElements = () => {
 
     // elements
-    let groupOne = document.querySelectorAll(`[id*="${elements1.id}-"][class*="${elements1.className}"]`)
-    let groupTwo = document.querySelectorAll(`[id*="${elements2.id}-"][class*="${elements2.className}"]`)
+    let groupOne = document.querySelectorAll(`[id*="ele1-"][class*="elementClass"]`)
+    let groupTwo = document.querySelectorAll(`[id*="ele2-"][class*="elementClass"]`)
 
     // if function domNewElements is define
       if (domNewElements !== undefined) {
         // manipulate group one
         for (let i = groupOne.length - 1; i >= 0; i--) {
-          groupOne[i].style.background = 'yellow'
-          if (i <= 10) {
-            groupOne[i].style.background = 'green'
-          }
+
+          groupOne[i].style.width = '100%'
+          groupOne[i].style.height = '100%'
+
           if (i >= 10 && i <= 20) {
             groupOne[i].style.background = 'orange'
           }
-          groupOne[20].style.background = 'maroon'
-          groupOne[30].style.background = 'maroon'
-          groupOne[40].style.background = 'maroon'
+
         }
+
         // manipulate group two
         for (let i = groupTwo.length - 1; i >= 0; i--) {
+
           groupTwo[i].style.background = 'red'
+
           if (i <= 10) {
             groupTwo[i].style.background = 'blue'
           }
-          groupTwo[20].style.background = 'maroon'
-          groupTwo[30].style.background = 'maroon'
-          groupTwo[40].style.background = 'maroon'
         }
-      } else {
-        // return false and log
-        console.log('Something went wrong!')
-        return false;
       }
+
       const elementStylesheet = document.createElement('style')
       elementStylesheet.innerHTML = `
-              .dynamicElement {
-                  height: 100px;
-                  width: 100%;
-              };
-              #unit-red-1 {
-                  background: ;
-              };
-          `;
-      let currentScriptTag = document.querySelector('script');
-      currentScriptTag.parentNode.insertBefore(elementStylesheet, currentScriptTag);
+            .elementClass {
+                height: 100px;
+                width: 100%;
+            };
+      `;
+
+      let currentScriptTag = document.querySelector('script')
+      currentScriptTag.parentNode.insertBefore(elementStylesheet, currentScriptTag)
 
     } // const domGetElements = () => {
 
