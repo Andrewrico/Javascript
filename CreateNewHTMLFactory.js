25b433b2c7ef9f0a879d2b06ebb9a7a72f67f988
andrewrico <andrewric86@gmail.com>
2021-04-02 22:14:39 -0400

    updates

--- CreateNewHTMLFactory.js
+++ CreateNewHTMLFactory.js
@@ -36,9 +36,7 @@ window.addEventListener('load', () => {
 
     // get fragment
     let newFragment = new DocumentFragment()
-
     // if (newFragment !== undefined) {
-
     // Create HTML
     newProtoObj.prototype.createNewHTML = function() {
         let newElement = [];
@@ -49,7 +47,6 @@ window.addEventListener('load', () => {
           newFragment.append(newElement);
         };
     }
-
     newGroupObj.prototype.createNewGroup = function() {
         createGroup = new newProtoObj(  
           count = `${this.count}`,   
@@ -61,44 +58,36 @@ window.addEventListener('load', () => {
 
     createGroup1 = new newGroupObj(100,  'div', 'element-1', 'group-1 column-2').createNewGroup();
     createGroup2 = new newGroupObj(100,  'div', 'element-2', 'group-2 column-2').createNewGroup();
-
     // return fragment with groups
     return newFragment;
     // } 
   }
-
   // if function domNewElements is defined return function domGetElements
   if (domNewElements !== undefined || domNewElements !== null) 
   document.querySelector(`#${factoryId}`).append(domNewElements());
-  
   // get elements
   const domGetElements = () => {
     // elements
     // if function domNewElements is define
-    // if (domNewElements !== undefined) {
-
-    //   let group1 = document.querySelectorAll(`[id*=element-1][class*="group-1 column-2"]`);
-    //   let group2 = document.querySelectorAll(`[id*=element-2][class*="group-2 column-2"]`);
-
-    //     // style group one
-    //     // for (let i = group1.length - 1; i >= 0; i--) {
-    //     //   [i].style.width = '100%'
-    //     //   [i].style.height = '100%'
-    //     //   if (i >= 10 && i <= 20) {
-    //     //     [i].style.background = 'orange'
-    //     //   }
-    //     // }
-
-    //     // style group two
-    //     // for (let i = group2.length - 1; i >= 0; i--) {
-    //     //   [i].style.background = 'red'
-    //     //   if (i <= 10) {
-    //     //     [i].style.background = 'blue'
-    //     //   }
-    //     // }
-
-    // }
-
+    if (domNewElements !== undefined) {
+      let group1 = document.querySelectorAll(`[id*=element-1][class*="group-1 column-2"]`);
+      let group2 = document.querySelectorAll(`[id*=element-2][class*="group-2 column-2"]`);
+        // style group one
+        for (let i = group1.length - 1; i >= 0; i--) {
+          [i].style.width = '100%'
+          [i].style.height = '100%'
+          if (i >= 10 && i <= 20) {
+            [i].style.background = 'orange'
+          }
+        }
+        // style group two
+        for (let i = group2.length - 1; i >= 0; i--) {
+          [i].style.background = 'red'
+          if (i <= 10) {
+            [i].style.background = 'blue'
+          }
+        }
+      }
       const elementStylesheet = document.createElement('style')
       elementStylesheet.innerHTML = `
             [class*="group-"] {
@@ -106,13 +95,9 @@ window.addEventListener('load', () => {
                 width: 100%;
             };
       `;
-      
       let currentScriptTag = document.querySelector('script')
       currentScriptTag.parentNode.insertBefore(elementStylesheet, currentScriptTag)
-
 } // const domGetElements = () => {
-
     // if function domNewElements is defined return function domGetElements
     if (domNewElements !== undefined || domNewElements !== null) domGetElements();
-
 });
